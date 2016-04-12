f0c27fb48ec0e61e64915cd76ba1343a1cc691b6
George Angel <george.angel.87@gmail.com>
2016-04-12 10:52:15 +0100

    Init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Upgrade script for CoreOS
+
+Gets downloaded for the `os-upgrade.service` in the cluster.
+
+To run the upgrade of the cluster:
+
+```sh
+fleetctl start os-upgrade.service
+```
---
+++ os-upgrade.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# DO NOT MOVE THIS FILE. IT GETS PULLED FROM RAW GITHUB LOCATION
+
+# Start update-engine
+#systemctl start update-engine.service
+
+# Check for updates
+update_engine_client -check_for_update
+
+# Random 10M delay before restarting (trying to stagger the restarts)
+delay=`/usr/bin/expr $RANDOM % 600`
+rebootflag='NEED_REBOOT'
+
+# Sleep 120S waiting for download
+sleep 120
+
+if update_engine_client -status | grep $rebootflag;
+then
+echo -n "etcd is "
+    if systemctl is-active etcd;
+    then
+        echo "Update reboot with locksmithctl."
+        locksmithctl reboot
+    else
+        echo "Update reboot in $delay seconds."
+        sleep $delay
+        reboot
+    fi
+fi
+exit 0
+
